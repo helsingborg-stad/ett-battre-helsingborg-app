c2deccd532a62cd01a219c3d67bdb8a5440eebb4
Jonatan Hanson <jonatan.hanson@gmail.com>
2023-06-29 15:25:29 +0200

    wrap webview in safeareaview

--- screens/Form.tsx
+++ screens/Form.tsx
@@ -1,6 +1,7 @@
 import Constants from 'expo-constants';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { SafeAreaView } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 import Container from '../components/common/Container';
@@ -8,23 +9,24 @@ import Container from '../components/common/Container';
 const Form: React.FC = () => {
   return (
     <Container>
-      <WebView
-        originWhitelist={['*']}
-        source={{
-          uri: Constants.expoConfig.extra.webviewUrl,
-        }}
-        javaScriptEnabled
-        domStorageEnabled
-        geolocationEnabled
-        allowFileAccess
-        cacheEnabled={false}
-        allowsFullscreenVideo
-        style={{
-          flex: 1,
-          backgroundColor: 'transparent',
-        }}
-      />
-
+      <SafeAreaView style={{ flex: 1 }}>
+        <WebView
+          originWhitelist={['*']}
+          source={{
+            uri: Constants.expoConfig.extra.webviewUrl,
+          }}
+          javaScriptEnabled
+          domStorageEnabled
+          geolocationEnabled
+          allowFileAccess
+          cacheEnabled={false}
+          allowsFullscreenVideo
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+          }}
+        />
+      </SafeAreaView>
       <StatusBar style="dark" />
     </Container>
   );
