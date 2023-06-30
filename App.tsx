d1653af5d248bcd4b65b180e38b88f48bd1d4288
Jonatan Hanson <jonatan.hanson@gmail.com>
2023-06-30 14:16:35 +0200

    implemented splash screen and loaded Helsingborg sans font

--- App.tsx
+++ App.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useCallback } from 'react';
+import { View, StyleSheet } from 'react-native';
 
 import AppNavigator from './navigation/AppNavigator';
 
-const App = () => {
-  return <AppNavigator />;
-};
+SplashScreen.preventAutoHideAsync();
 
-export default App;
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Helsingborg-Sans-Bold': require('./assets/fonts/helsingborg_sans/OTF/HelsingborgSans-Bold.otf'),
+    'Helsingborg-Sans-Light': require('./assets/fonts/helsingborg_sans/OTF/HelsingborgSans-Light.otf'),
+    'Helsingborg-Sans-Medium': require('./assets/fonts/helsingborg_sans/OTF/HelsingborgSans-Medium.otf'),
+    'Helsingborg-Sans-Regular': require('./assets/fonts/helsingborg_sans/OTF/HelsingborgSans-Regular.otf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      //await new Promise((resolve) => setTimeout(resolve, 500));
+
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <AppNavigator />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
