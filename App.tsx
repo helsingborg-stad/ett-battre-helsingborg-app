5bdc380be0a195dfe63d41ac034f6e207ad9c26a
Jonatan Hanson <jonatan.hanson@gmail.com>
2023-07-04 14:31:34 +0200

    added roboto font

--- App.tsx
+++ App.tsx
@@ -9,10 +9,14 @@ SplashScreen.preventAutoHideAsync();
 
 const App: React.FC = () => {
   const [fontsLoaded] = useFonts({
-    'Helsingborg-Sans-Bold': require('./assets/fonts/helsingborg_sans/OTF/HelsingborgSans-Bold.otf'),
     'Helsingborg-Sans-Light': require('./assets/fonts/helsingborg_sans/OTF/HelsingborgSans-Light.otf'),
-    'Helsingborg-Sans-Medium': require('./assets/fonts/helsingborg_sans/OTF/HelsingborgSans-Medium.otf'),
     'Helsingborg-Sans-Regular': require('./assets/fonts/helsingborg_sans/OTF/HelsingborgSans-Regular.otf'),
+    'Helsingborg-Sans-Medium': require('./assets/fonts/helsingborg_sans/OTF/HelsingborgSans-Medium.otf'),
+    'Helsingborg-Sans-Bold': require('./assets/fonts/helsingborg_sans/OTF/HelsingborgSans-Bold.otf'),
+    'Roboto-Light': require('./assets/fonts/roboto/Roboto-Light.ttf'),
+    'Roboto-Regular': require('./assets/fonts/roboto/Roboto-Regular.ttf'),
+    'Roboto-Medium': require('./assets/fonts/roboto/Roboto-Medium.ttf'),
+    'Roboto-Bold': require('./assets/fonts/roboto/Roboto-Bold.ttf'),
   });
 
   const onLayoutRootView = useCallback(async () => {
