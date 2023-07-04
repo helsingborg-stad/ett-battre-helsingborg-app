71a957f6a532688e0f06a749be379d838bf84678
Jonatan Hanson <jonatan.hanson@gmail.com>
2023-07-04 14:36:15 +0200

    changed from stack navigation to bottom tab navigation

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -1,32 +1,37 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 
 import Form from '../screens/Form';
 import Home from '../screens/Home';
 import { RootStackParamList } from '../types/Types';
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator<RootStackParamList>();
 
 const AppNavigator = (): JSX.Element => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          tabBarStyle: { display: 'none' },
+        }}>
+        <Tab.Screen
           name="Home"
           component={Home}
           options={{
             headerShown: false,
           }}
         />
-        <Stack.Screen
+        <Tab.Screen
           name="Form"
           component={Form}
           options={{
             title: 'Lämna synpunkt eller felanmälan',
+            lazy: false, // Pre loads webview in the background. Currently not working on iOS beacuse of a bug.
           }}
         />
-      </Stack.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
